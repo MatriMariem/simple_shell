957841fb7c18437557f7094882b76b888b486317
Mariem Matri <ilyesjihenmeriem@yahoo.fr>
2019-11-28 04:40:06 -0800

    exit with parameter

--- buildin.c
+++ buildin.c
@@ -6,12 +6,20 @@
  */
 void exitt(char **arv)
 {
-	int i;
+	int i, n;
 
+	if (arv[1])
+	{
+		n = _atoi(arv[1]);
+		if (n <= -1)
+			n = 2;
+		freearv(arv);
+		exit(n);
+	}
 	for (i = 0; arv[i]; i++)
 		free(arv[i]);
-		free(arv);
-		exit(0);
+	free(arv);
+	exit(0);
 }
 
 /**
